cc29c6f414189a9537d96daf9edbf21a9c10a11b
M Pitman <49458516+pitmanme@users.noreply.github.com>
2022-01-28 17:04:48 -0800

    Add files via upload

---
+++ PCA/cenpc_PCA_analysis.py
@@ -0,0 +1,306 @@
+from math import *
+from numpy import *
+from matplotlib import *
+from pylab import *
+import MDAnalysis
+import numpy
+import numpy.linalg
+import matplotlib.pyplot as plt
+import numpy as np
+
+import matplotlib
+
+font = {'family' : 'arial',
+    'weight' : 'medium',
+    'size'   : 18}
+
+matplotlib.rc('font', **font)
+################################################################################# 
+
+get_cmap("jet")
+
+timestep = 1
+print "start loading file ...."
+pos_ca_atoms_xyz = numpy.loadtxt('kac_positions_ca_atoms.output', unpack=True)
+print "file loaded"
+length_ca_atoms_xyz = len(pos_ca_atoms_xyz)
+n_frames = len(pos_ca_atoms_xyz[0,:])
+print "n_frames: ", n_frames
+print "length_ca_atoms_xyz: ", length_ca_atoms_xyz
+print "pos_ca_atoms_xyz: ", shape(pos_ca_atoms_xyz)
+
+
+T = pos_ca_atoms_xyz.T
+print "calculating covariance matrix ..."
+C = numpy.cov(T,rowvar=0)
+
+#print C
+#print shape(C)
+
+M,L = numpy.linalg.eig(C)
+print M
+print L
+
+eigenvalues = numpy.zeros(length_ca_atoms_xyz)
+eig_variance = numpy.zeros(length_ca_atoms_xyz)
+variance_sum = 0
+
+for i in range(length_ca_atoms_xyz):
+    eigenvalues[i] = M[i]
+
+eigenvalues.sort()
+
+eigenvalues = eigenvalues[::-1]
+
+for i in range(length_ca_atoms_xyz):
+    variance_sum += eigenvalues[i]
+    print i, variance_sum, eigenvalues[i]
+
+print "total variance: ", variance_sum
+
+for i in range(length_ca_atoms_xyz):
+    eigenvalues[i] /= variance_sum
+
+for i in range(length_ca_atoms_xyz):
+    eig_variance[i] = numpy.sum(eigenvalues[0:i+1])
+
+print "eigenvalues (normalized): ", eigenvalues
+print "eig_variance: ", eig_variance
+
+eig_indices = range(length_ca_atoms_xyz)
+
+#
+# variance plot
+#
+#plt.figure()
+#plt.plot(eig_indices, eigenvalues)
+#plt.xscale('log')
+#plt.yscale('log')
+#plt.xlabel('Principal Component')
+#plt.ylabel('Variance (%)')
+#plt.show()
+
+
+n = 50
+
+
+m1 = L[:,0]
+m2 = L[:,1]
+print 'm1: ',m1
+print 'm2: ',m2
+
+#f_pc1 = open('pc1_ca_atoms_xyz.output','w')
+#f_pc2 = open('pc2_ca_atoms_xyz.output','w')
+#
+#for i in range(length_ca_atoms_xyz):
+#    f_pc1.write(str(m1[i]) + '\n')
+#    f_pc2.write(str(m2[i]) + '\n')
+#
+#f_pc1.close()
+#f_pc2.close()
+
+#print shape(T)
+#print m1
+
+v_0 = dot(T,m1)
+v_1 = dot(T,m2)
+
+#print v_0
+
+kb_T = 0.0019872041*300
+
+a = zeros([n+1,n+1])
+
+v_0_new = array(v_0)
+v_1_new = array(v_1)
+
+v_0_new -= numpy.float(numpy.mean(v_0_new))
+v_1_new -= numpy.float(numpy.mean(v_1_new))
+
+min_0 = float(min(v_0_new))
+min_1 = float(min(v_1_new))
+
+max_0 = float(max(v_0_new))
+max_1 = float(max(v_1_new))
+
+print "PCA info [min_0, max_0, min_1, max_1] : " + "[" + str(min_0) + ", " + str(max_0) + ", " + str(min_1) + ", " + str(max_1) + "]." 
+
+dr_1 = float((max_1 - min_1)/n)
+dr_0 = float((max_0 - min_0)/n)
+
+
+
+for i in range(size(v_0_new)):
+    i_pc1 = int((v_0_new[i]-min_0)/dr_0)
+    i_pc2 = int((v_1_new[i]-min_1)/dr_1)
+    a[i_pc1][i_pc2] += 1
+
+v0 = linspace(min_0, max_0, n+1)
+v1 = linspace(min_1, max_1, n+1) 
+
+sum_a = a.sum()
+
+#print sum_a
+
+F_min = 100
+F_max = 0
+
+for i in range(n+1):
+    for j in range(n+1):
+        if a[i][j] != 0:
+            a[i][j] = -1*kb_T*log(a[i][j]/sum_a)
+            if a[i][j] < F_min:
+                F_min = a[i][j]
+            if a[i][j] > F_max:
+                F_max = a[i][j]
+
+for i in range(n+1):
+    for j in range(n+1):
+        if a[i][j] == 0:
+            a[i][j] = None
+
+
+print "F_min: " + str(F_min) + " kT."
+print "F_max: " + str(F_max) + " kT."
+
+levels = (F_max - F_min)/0.5
+
+print levels
+
+a = a - F_min
+neg_diff = (-1)*(F_min)
+a[a==neg_diff] = None
+#
+# free energy projection
+# vmin = 0, vmax = 3.0
+fig, ax0 = plt.subplots(ncols=6, sharey=True, sharex=True)
+plt.figure()
+plt.contourf(v0,v1,a,20, cmap='jet', vmin=0.0, vmax=4.2)
+plt.title("CENP-C + CENP-A, 300 K")
+plt.xlabel("PC1")
+plt.ylabel("PC2")
+plt.xlim((-40.0,40.0))
+plt.ylim((-40.0,40.0))
+plt.colorbar()
+plt.show()
+"""
+##########################################################
+#
+#       Calculate basin representative snapshots
+#
+##########################################################
+
+box_1_frames = []
+box_1_xlimits = [0, 5]
+box_1_ylimits = [8.0, 12.0]
+
+box_2_frames = []
+box_2_xlimits = [-1.0, 5.0]
+box_2_ylimits = [-15.0, -12.0]
+
+for i in range(size(v_0_new)):
+    if ( (box_1_xlimits[0] < v_0_new[i] < box_1_xlimits[1]) and (box_1_ylimits[0] < v_1_new[i] < box_1_ylimits[1]) ):
+        box_1_frames.append(i)
+
+for j in range(size(v_0_new)):
+    if ( (box_2_xlimits[0] < v_0_new[j] < box_2_xlimits[1]) and (box_2_ylimits[0] < v_1_new[j] < box_2_ylimits[1]) ):
+        box_2_frames.append(j)
+
+# create universe                                                                                                                                                         
+gro_file = 'kac_t0.gro'
+xtc_file = 'kac_trunc.xtc'
+print 'hello'
+u = MDAnalysis.Universe(gro_file, xtc_file)
+protein_or_dna_atoms = u.selectAtoms("bynum 1:11568 or nucleic")
+print protein_or_dna_atoms
+
+# user-defined variables                                                                                                                                                  
+skip = 0
+cut = 0
+start = cut + 1 # to account for the intial gro file structure                                                                                                            
+timestep = 1 # in frames                                                                                                                                                  
+N_traj = len(u.trajectory) - 1 # taking away intial gro                                                                                                                   
+print "N_traj: ", N_traj # check this as trajectory length (200000 if none skipped)                                                                                        
+
+# select C-alphas and initiate geometric average                                                                                                                          
+ca_atoms = u.selectAtoms('name CA')
+ca_length = len(ca_atoms)
+ca_array = range(len(ca_atoms))
+
+box1_average = np.zeros((ca_length,3))
+box1_length = len(box_1_frames)
+
+box2_average = np.zeros((ca_length,3))
+box2_length = len(box_2_frames)
+
+# calculate geometric average                                                                                                                                             
+for ts in u.trajectory[start::timestep]:
+    t = ts.frame - 1
+    if t in box_1_frames:
+        print t
+        for i in ca_array:
+            box1_average[i][0] += ca_atoms[i].pos[0]/float(box1_length)
+            box1_average[i][1] += ca_atoms[i].pos[1]/float(box1_length)
+            box1_average[i][2] += ca_atoms[i].pos[2]/float(box1_length)
+
+for ts in u.trajectory[start::timestep]:
+    t = ts.frame - 1
+    if t in box_2_frames:
+        print t
+        for i in ca_array:
+            box2_average[i][0] += ca_atoms[i].pos[0]/float(box2_length)
+            box2_average[i][1] += ca_atoms[i].pos[1]/float(box2_length)
+            box2_average[i][2] += ca_atoms[i].pos[2]/float(box2_length)
+
+# calculate structure with the lowest RMSD to the geometric average                                                                                                       
+rmsd_min_1 = 1000.0 # rediculously high                                                                                                                                   
+rmsd_min_index_1 = 2000000 # rediculously high                                                                                                                            
+
+rmsd_min_2 = 1000.0 # rediculously high                                                                                                                                   
+rmsd_min_index_2 = 2000000 # rediculously high
+
+for ts in u.trajectory[start::timestep]:
+    t = ts.frame - 1
+    if t in box_1_frames:
+        print t
+        rmsd = 0.0
+        sum = 0.0
+        for i in ca_array:
+            sum += pow((box1_average[i][0] - ca_atoms[i].pos[0]), 2)
+            sum += pow((box1_average[i][1] - ca_atoms[i].pos[1]), 2)
+            sum += pow((box1_average[i][2] - ca_atoms[i].pos[2]), 2)
+            print 'sum, ', sum
+            rmsd = np.sqrt(sum)/ca_length
+            if (rmsd < rmsd_min_1):
+                rmsd_min_1 = rmsd
+                rmsd_min_index_1 = ts.frame
+
+for ts in u.trajectory[start::timestep]:
+    t = ts.frame - 1
+    if t in box_2_frames:
+        print t
+        rmsd = 0.0
+        sum = 0.0
+        for i in ca_array:
+            sum += pow((box2_average[i][0] - ca_atoms[i].pos[0]), 2)
+            sum += pow((box2_average[i][1] - ca_atoms[i].pos[1]), 2)
+            sum += pow((box2_average[i][2] - ca_atoms[i].pos[2]), 2)
+            print 'sum, ', sum
+            rmsd = np.sqrt(sum)/ca_length
+            if (rmsd < rmsd_min_2):
+                rmsd_min_2 = rmsd
+                rmsd_min_index_2 = ts.frame
+
+# output information about the average structure and the PDB, average_structure.pdb                                                                                       
+print 'box1 representative rmsd, frame_1: ', rmsd_min_1, rmsd_min_index_1
+print 'box2 representative rmsd, frame_2: ', rmsd_min_2, rmsd_min_index_2
+
+# write representative structures                                                                                                                                         
+for ts in u.trajectory[start::timestep]:
+    if (ts.frame == rmsd_min_index_1):
+        print ts.frame
+        protein_or_dna_atoms.write('box3_structure_w_dna.pdb')
+    elif (ts.frame == rmsd_min_index_2):
+        print ts.frame
+        protein_or_dna_atoms.write('box4_structure_w_dna.pdb')
+"""
---
+++ PCA/contr_PCA_file_gen.py
@@ -0,0 +1,261 @@
+from math import *
+from numpy import *
+from matplotlib import *
+from pylab import *
+import MDAnalysis
+import numpy
+import numpy.linalg
+import matplotlib.pyplot as plt
+import io
+
+
+u = MDAnalysis.Universe('/Users/mpitman/work/dt/nuc/cenpc/contr/structure_files/traj_t1_dry.gro','/Users/mpitman/work/dt/nuc/cenpc/contr/trajectories/md6/traj_md1_coarse.xtc')
+cut = 0 # times 25(frames skipped)*2(ps/frame) = 600000 ps cut                                                     
+start = cut + 1
+timestep = 1
+n_frames = len(u.trajectory)-1						#This would make it appear that ^ is not the case
+print "n_frames: ", n_frames
+
+ca_atoms = u.selectAtoms('bynum 213:2787 and name CA') + u.selectAtoms('bynum 2947:8501 and name CA') + u.selectAtoms('bynum 8642:10058 and name CA') + u.selectAtoms('bynum 10240:11552 and name CA') + u.selectAtoms('bynum 12337:15389 and name P') + u.selectAtoms('bynum 16979:20031 and name P')
+print "len(ca_atoms): ", len(ca_atoms)
+print ca_atoms
+
+# calculate com_total
+tot_com = numpy.zeros((n_frames,3))					#return a new array of given size, n_frames rows and 3 columns, filled with 0s
+for ts in u.trajectory[start::timestep]:                                #[start::timestep]                                                                                                                                     
+    n = ts.frame - 2                                                    #why go two timesteps back?  Is this because the first was removed and then shifted to 0?                                                                                                                              
+    print n
+    for ti in range(len(ca_atoms)):
+        tot_com[n][0] += ca_atoms[ti].pos[0]                            #first column, x values of the position.. average position looped through and sequential sum?                                                                                                                                
+        tot_com[n][1] += ca_atoms[ti].pos[1]                                                                                                                                                            
+        tot_com[n][2] += ca_atoms[ti].pos[2]                                                                                                                                                            
+    tot_com[n][0] /= numpy.float(len(ca_atoms))                         #c /= a is equivalent to c = c/a, why is float included here?                                                                                                                               
+    tot_com[n][1] /= numpy.float(len(ca_atoms))                         #this inner for loop is giving the center of mass of indiv. frames > deposits to matrix                                                                                                                                
+    tot_com[n][2] /= numpy.float(len(ca_atoms))   
+
+
+# calculate PCA input file, CA positions with respect to COM
+f_ca_atoms = open('contr_p_ca_t.output','w')                      #a new file is being generated
+for ts in u.trajectory[start::timestep]:
+    t = ts.frame - 2
+    print "t:", t
+    for i in range(len(ca_atoms)):
+	print "i:", i
+        ca_x = ca_atoms[i].pos[0] - tot_com[t][0]
+        ca_y = ca_atoms[i].pos[1] - tot_com[t][1]
+        ca_z = ca_atoms[i].pos[2] - tot_com[t][2]
+        position_string = str(ca_x) + ' ' + str(ca_y) + ' ' + str(ca_z) + ' '
+        f_ca_atoms.write(position_string)
+    f_ca_atoms.write('\n')
+f_ca_atoms.close()
+
+"""beginning
+
+for i in range(len(ca_atoms)):
+    with io.open("position_ca_" + str(i) + ".dat", 'w', encoding='utf-8') as f:
+       f.write(str(func(i))
+
+
+
+A_ca_atoms = ca_atoms[0:89]
+B_ca_atoms = ca_atoms[89:167]
+C_ca_atoms = ca_atoms[167:264]
+D_ca_atoms = ca_atoms[264:354]
+E_ca_atoms = ca_atoms[354:442]
+F_ca_atoms = ca_atoms[442:521]
+G_ca_atoms = ca_atoms[521:621]
+H_ca_atoms = ca_atoms[621:711]
+#print A_ca_atoms
+#print A_ca_atoms[0]
+#print A_ca_atoms[88]
+#print B_ca_atoms[0]
+#print len(A_ca_atoms), len(A_ca_atoms)+len(B_ca_atoms), len(A_ca_atoms)+len(B_ca_atoms)+len(C_ca_atoms), len(A_ca_atoms)+len(B_ca_atoms)+len(C_ca_atoms)+len(D_ca_atoms), len(A_ca_atoms)+len(B_ca_atoms)+len(C_ca_atoms)+len(D_ca_atoms)+len(E_ca_atoms), len(A_ca_atoms)+len(B_ca_atoms)+len(C_ca_atoms)+len(D_ca_atoms)+len(E_ca_atoms)+len(F_ca_atoms),len(A_ca_atoms)+len(B_ca_atoms)+len(C_ca_atoms)+len(D_ca_atoms)+len(E_ca_atoms)+len(F_ca_atoms)+len(G_ca_atoms),len(A_ca_atoms)+len(B_ca_atoms)+len(C_ca_atoms)+len(D_ca_atoms)+len(E_ca_atoms)+len(F_ca_atoms)+len(G_ca_atoms)+len(H_ca_atoms)
+
+total_length = len(A_ca_atoms)+len(B_ca_atoms)+len(C_ca_atoms)+len(D_ca_atoms)+len(E_ca_atoms)+len(F_ca_atoms)+len(G_ca_atoms)+len(H_ca_atoms)
+
+print "check: ",(total_length==711)
+
+tot_com = numpy.zeros((n_frames,3))
+A_com = numpy.zeros((n_frames,3))
+B_com = numpy.zeros((n_frames,3))
+C_com = numpy.zeros((n_frames,3))
+D_com = numpy.zeros((n_frames,3))
+E_com = numpy.zeros((n_frames,3))
+F_com = numpy.zeros((n_frames,3))
+G_com = numpy.zeros((n_frames,3))
+H_com = numpy.zeros((n_frames,3))
+
+
+# calculate center-of-mass matrices
+for ts in u.trajectory[0::timestep]:
+    n = ts.frame - 1
+    print n
+    
+    for ti in range(len(ca_atoms)):
+        tot_com[n][0] += ca_atoms[ti].pos[0]
+        tot_com[n][1] += ca_atoms[ti].pos[1]
+        tot_com[n][2] += ca_atoms[ti].pos[2]
+    tot_com[n][0] /= numpy.float(len(ca_atoms))
+    tot_com[n][1] /= numpy.float(len(ca_atoms))
+    tot_com[n][2] /= numpy.float(len(ca_atoms))
+
+    for ai in range(len(A_ca_atoms)):
+        A_com[n][0] += A_ca_atoms[ai].pos[0]
+        A_com[n][1] += A_ca_atoms[ai].pos[1]
+        A_com[n][2] += A_ca_atoms[ai].pos[2]
+    A_com[n][0] /= numpy.float(len(A_ca_atoms))
+    A_com[n][1] /= numpy.float(len(A_ca_atoms))
+    A_com[n][2] /= numpy.float(len(A_ca_atoms))
+    
+    for bi in range(len(B_ca_atoms)):
+        B_com[n][0] += B_ca_atoms[bi].pos[0]
+        B_com[n][1] += B_ca_atoms[bi].pos[1]
+        B_com[n][2] += B_ca_atoms[bi].pos[2]
+    B_com[n][0] /= numpy.float(len(B_ca_atoms))
+    B_com[n][1] /= numpy.float(len(B_ca_atoms))
+    B_com[n][2] /= numpy.float(len(B_ca_atoms))
+
+    for ci in range(len(C_ca_atoms)):
+        C_com[n][0] += C_ca_atoms[ci].pos[0]
+        C_com[n][1] += C_ca_atoms[ci].pos[1]
+        C_com[n][2] += C_ca_atoms[ci].pos[2]
+    C_com[n][0] /= numpy.float(len(C_ca_atoms))
+    C_com[n][1] /= numpy.float(len(C_ca_atoms))
+    C_com[n][2] /= numpy.float(len(C_ca_atoms))
+
+    for di in range(len(D_ca_atoms)):
+        D_com[n][0] += D_ca_atoms[di].pos[0]
+        D_com[n][1] += D_ca_atoms[di].pos[1]
+        D_com[n][2] += D_ca_atoms[di].pos[2]
+    D_com[n][0] /= numpy.float(len(D_ca_atoms))
+    D_com[n][1] /= numpy.float(len(D_ca_atoms))
+    D_com[n][2] /= numpy.float(len(D_ca_atoms))
+
+    for ei in range(len(E_ca_atoms)):
+        E_com[n][0] += E_ca_atoms[ei].pos[0]
+        E_com[n][1] += E_ca_atoms[ei].pos[1]
+        E_com[n][2] += E_ca_atoms[ei].pos[2]
+    E_com[n][0] /= numpy.float(len(E_ca_atoms))
+    E_com[n][1] /= numpy.float(len(E_ca_atoms))
+    E_com[n][2] /= numpy.float(len(E_ca_atoms))
+
+    for fi in range(len(F_ca_atoms)):
+        F_com[n][0] += F_ca_atoms[fi].pos[0]
+        F_com[n][1] += F_ca_atoms[fi].pos[1]
+        F_com[n][2] += F_ca_atoms[fi].pos[2]
+    F_com[n][0] /= numpy.float(len(F_ca_atoms))
+    F_com[n][1] /= numpy.float(len(F_ca_atoms))
+    F_com[n][2] /= numpy.float(len(F_ca_atoms))
+
+    for gi in range(len(G_ca_atoms)):
+        G_com[n][0] += G_ca_atoms[gi].pos[0]
+        G_com[n][1] += G_ca_atoms[gi].pos[1]
+        G_com[n][2] += G_ca_atoms[gi].pos[2]
+    G_com[n][0] /= numpy.float(len(G_ca_atoms))
+    G_com[n][1] /= numpy.float(len(G_ca_atoms))
+    G_com[n][2] /= numpy.float(len(G_ca_atoms))
+
+    for hi in range(len(H_ca_atoms)):
+        H_com[n][0] += H_ca_atoms[hi].pos[0]
+        H_com[n][1] += H_ca_atoms[hi].pos[1]
+        H_com[n][2] += H_ca_atoms[hi].pos[2]
+    H_com[n][0] /= numpy.float(len(H_ca_atoms))
+    H_com[n][1] /= numpy.float(len(H_ca_atoms))
+    H_com[n][2] /= numpy.float(len(H_ca_atoms))
+
+# calculate d(COM[i],COM[j]), distances between centers-of-mass
+
+f_00 = open('d_COM_00.output','w')
+f_01 = open('d_COM_01.output','w')
+f_02 = open('d_COM_02.output','w')
+f_03 = open('d_COM_03.output','w')
+f_04 = open('d_COM_04.output','w')
+f_05 = open('d_COM_05.output','w')
+f_06 = open('d_COM_06.output','w')
+f_07 = open('d_COM_07.output','w')
+f_08 = open('d_COM_08.output','w')
+f_09 = open('d_COM_09.output','w')
+f_10 = open('d_COM_10.output','w')
+f_11 = open('d_COM_11.output','w')
+f_12 = open('d_COM_12.output','w')
+f_13 = open('d_COM_13.output','w')
+f_14 = open('d_COM_14.output','w')
+f_15 = open('d_COM_15.output','w')
+f_16 = open('d_COM_16.output','w')
+f_17 = open('d_COM_17.output','w')
+f_18 = open('d_COM_18.output','w')
+f_19 = open('d_COM_19.output','w')
+f_20 = open('d_COM_20.output','w')
+f_21 = open('d_COM_21.output','w')
+f_22 = open('d_COM_22.output','w')
+f_23 = open('d_COM_23.output','w')
+
+d_COM = numpy.zeros((n_frames,24))
+
+for t in range(n_frames):
+    print t
+    
+    
+    Ax_com = A_com[t][0]-tot_com[t][0]
+    Ay_com = A_com[t][1]-tot_com[t][1]
+    Az_com = A_com[t][2]-tot_com[t][2]
+    f_00.write(str(t) + ' ' + str(Ax_com) + '\n')
+    f_01.write(str(t) + ' ' + str(Ay_com) + '\n')
+    f_02.write(str(t) + ' ' + str(Az_com) + '\n')
+
+
+    Bx_com = B_com[t][0]-tot_com[t][0]
+    By_com = B_com[t][1]-tot_com[t][1]
+    Bz_com = B_com[t][2]-tot_com[t][2]
+    f_03.write(str(t) + ' ' + str(Bx_com) + '\n')
+    f_04.write(str(t) + ' ' + str(By_com) + '\n')
+    f_05.write(str(t) + ' ' + str(Bz_com) + '\n')
+    
+
+    Cx_com = C_com[t][0]-tot_com[t][0]
+    Cy_com = C_com[t][1]-tot_com[t][1]
+    Cz_com = C_com[t][2]-tot_com[t][2]
+    f_06.write(str(t) + ' ' + str(Cx_com) + '\n')
+    f_07.write(str(t) + ' ' + str(Cy_com) + '\n')
+    f_08.write(str(t) + ' ' + str(Cz_com) + '\n')
+
+
+    Dx_com = D_com[t][0]-tot_com[t][0]
+    Dy_com = D_com[t][1]-tot_com[t][1]
+    Dz_com = D_com[t][2]-tot_com[t][2]
+    f_09.write(str(t) + ' ' + str(Dx_com) + '\n')
+    f_10.write(str(t) + ' ' + str(Dy_com) + '\n')
+    f_11.write(str(t) + ' ' + str(Dz_com) + '\n')
+
+
+    Ex_com = E_com[t][0]-tot_com[t][0]
+    Ey_com = E_com[t][1]-tot_com[t][1]
+    Ez_com = E_com[t][2]-tot_com[t][2]
+    f_12.write(str(t) + ' ' + str(Ex_com) + '\n')
+    f_13.write(str(t) + ' ' + str(Ey_com) + '\n')
+    f_14.write(str(t) + ' ' + str(Ez_com) + '\n')
+
+
+    Fx_com = F_com[t][0]-tot_com[t][0]
+    Fy_com = F_com[t][1]-tot_com[t][1]
+    Fz_com = F_com[t][2]-tot_com[t][2]
+    f_15.write(str(t) + ' ' + str(Fx_com) + '\n')
+    f_16.write(str(t) + ' ' + str(Fy_com) + '\n')
+    f_17.write(str(t) + ' ' + str(Fz_com) + '\n')
+
+    Gx_com = G_com[t][0]-tot_com[t][0]
+    Gy_com = G_com[t][1]-tot_com[t][1]
+    Gz_com = G_com[t][2]-tot_com[t][2]
+    f_18.write(str(t) + ' ' + str(Gx_com) + '\n')
+    f_19.write(str(t) + ' ' + str(Gy_com) + '\n')
+    f_20.write(str(t) + ' ' + str(Gz_com) + '\n')
+
+    Hx_com = H_com[t][0]-tot_com[t][0]
+    Hy_com = H_com[t][1]-tot_com[t][1]
+    Hz_com = H_com[t][2]-tot_com[t][2]
+    f_21.write(str(t) + ' ' + str(Hx_com) + '\n')
+    f_22.write(str(t) + ' ' + str(Hy_com) + '\n')
+    f_23.write(str(t) + ' ' + str(Hz_com) + '\n')
+    
+# check individual files after this script runs
+end"""
